732827b4ecde53ce773decae3ec80a91d0e553e1
ubuntu <ubuntu@ubuntu.(none)>
2013-10-27 02:22:01 -0200

    Server now can check the login, get the avatar list and save avatars at the DataBase.
    Signed-off-by: Tales Martins de Souza <tales.martins@gmail.com>

---
+++ server/core/Avatar.h
@@ -0,0 +1,20 @@
+#include <string>
+#include <cstring>
+
+typedef struct _Avatar
+{
+	int idavatar;
+	char* name;
+	int level;
+	char sex;
+	int maxhp;
+	int hp;
+	int maxmana;
+	int mana;
+	int positionx;
+	int positiony;
+	int positionz;
+	int rotationx;
+	int rotationy;
+	int rotationz;
+} Avatar;
